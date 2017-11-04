8c75ac5c2da14e21ee2cfcaa00830f12bcd4e718
viettien <viettienn97@gmail.com>
2017-11-05 01:45:37 +0700

    update14

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'users/new'
+
 root 'static_pages#home'
 get  '/help',    to: 'static_pages#help'
 get  '/home',    to: 'static_pages#home'
 get  '/signup',  to: 'users#new'
+post '/signup',  to: 'users#create'
+get    '/login',   to: 'sessions#new'
+post   '/login',   to: 'sessions#create'
+delete '/logout',  to: 'sessions#destroy'
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+resources :microposts,          only: [:create, :destroy]
+resources :relationships,       only: [:create, :destroy]
 end
