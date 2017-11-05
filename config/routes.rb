b7a5ffcec2bfe13d8c71c1a027015ba8e9e8a2fb
viettien <viettienn97@gmail.com>
2017-11-05 12:58:06 +0700

    update project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+root 'static_pages#home'
+get  '/help',    to: 'static_pages#help'
+get  '/home',    to: 'static_pages#home'
+get  '/signup',  to: 'users#new'
+post '/signup',  to: 'users#create'
+get    '/login',   to: 'sessions#new'
+post   '/login',   to: 'sessions#create'
+delete '/logout',  to: 'sessions#destroy'
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+resources :microposts,          only: [:create, :destroy]
+resources :relationships,       only: [:create, :destroy]
+resources :microposts do
+  resources :comments
+end
 end
